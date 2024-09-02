049c6ca0943da3be8cbd88ee7ab3c60891eba8e8
WAHU0508 <sharongikenye@gmail.com>
2024-09-02 12:00:46 +0300

    Countdown functions with sleep and without sleep

--- lib/countdown.py
+++ lib/countdown.py
@@ -1,1 +1,21 @@
 # your code goes here!
+import time
+
+def countdown(n):
+    number = n
+    while number >= 0:
+        if number > 0:
+            print(f"{number} SECOND(S)!")
+        if number == 0:
+            print("HAPPY NEW YEAR!")
+        number -= 1
+def countdown_with_sleep(n):
+    number = n
+    while number >= 0:
+        if number > 0:
+            print(f"{number} SECOND(S)!")
+            time.sleep(1)
+        if number == 0:
+            print("HAPPY NEW YEAR!")
+        number -= 1
+countdown_with_sleep(10)
